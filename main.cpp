9a6f5fd7c9ac6c6db3e48ceab5ccf80be47db69a
BobrovaVarya <barrbaraa94@gmail.com>
2014-10-24 10:12:48 +0400

    пункт 1.5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "sdt.h"
 int main ()
 {
-    double a,b;
+    double a,b,s;
     cout <<"Enter two numbers \n";
     cin>>a;
     cin>>b;
+ //double s;
+ s=a+b;
+ cout<<"Sum= "<<s<<" \n";
 
 }
