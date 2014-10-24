c894ab4daf00e27f9eab8817384b568beaadcada
BobrovaVarya <barrbaraa94@gmail.com>
2014-10-24 11:58:36 +0400

    m

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 //учебная программа
 int main ()
 {
-    double a,b,s,d;
+    double a,b,s,d,m;
     cout <<"Enter two numbers \n";
     cin>>a;
     cin>>b;
     s=a+b;
    d=a-b;
-   cout<<"S= "<<s<<" \n";
-   cout<<"D= "<<d<<" \n";
+   m=a*b;
+   cout<<"S= "<<s<<" \n"
+   <<"D= "<<d<<" \n"
+   <<"M= "<<m<<" \n";
 
 }
