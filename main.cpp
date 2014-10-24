85f7386316420498c107d564b49d1f151597b749
BobrovaVarya <barrbaraa94@gmail.com>
2014-10-24 11:01:06 +0400

    2.5.1

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "sdt.h"
 int main ()
 {
-    double a,b,s;
+    double a,b,s,d;
     cout <<"Enter two numbers \n";
     cin>>a;
     cin>>b;
- //double s;
- s=a+b;
- cout<<"Sum= "<<s<<" \n";
+    s=a+b;
+   d=a-b;
+   cout<<"S= "<<s<<" \n";
+   cout<<"D= "<<d<<" \n";
 
 }
